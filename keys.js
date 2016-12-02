cc826fc0df0453a3d479a794c8b9d1daf999d46b
ejpelagio <ejpelagio@yahoo.com>
2016-12-01 22:52:39 -0600

    files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<fZ2stiYylhGWyyLcCn9InA0Ck>',
+  consumer_secret: '<vmhtRPrALPgVwwU3age48GtpIOjHK0bl9EHG7dcqk07Wt740Aa>',
+  access_token_key: '<191928620-ALPPrHTim6TMrlpnQ3Qk6asbGvV8eu767Xozgez8>',
+  access_token_secret: '<d2tBp7MqKVFAH4jjXdchglTNEuDc65Klo1jRONlg4qj7p>',
+}
