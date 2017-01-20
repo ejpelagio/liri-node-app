489a0bc3aee3feea93d6e3f20215de48b47da0f2
ejpelagio <ejpelagio@yahoo.com>
2017-01-20 11:29:27 -0600

    update

--- liri.js
+++ liri.js
@@ -18,15 +18,15 @@ var keys = require("./keys.js");
 
 /*
 var client = new Twitter ({
-		  consumer_key: '<fZ2stiYylhGWyyLcCn9InA0Ck>',
-		  consumer_secret: '<vmhtRPrALPgVwwU3age48GtpIOjHK0bl9EHG7dcqk07Wt740Aa>',
-		  access_token_key: '<191928620-ALPPrHTim6TMrlpnQ3Qk6asbGvV8eu767Xozgez8>',
-		  access_token_secret: '<d2tBp7MqKVFAH4jjXdchglTNEuDc65Klo1jRONlg4qj7p>',
+      consumer_key: '<fZ2stiYylhGWyyLcCn9InA0Ck>',
+      consumer_secret: '<vmhtRPrALPgVwwU3age48GtpIOjHK0bl9EHG7dcqk07Wt740Aa>',
+      access_token_key: '<191928620-ALPPrHTim6TMrlpnQ3Qk6asbGvV8eu767Xozgez8>',
+      access_token_secret: '<d2tBp7MqKVFAH4jjXdchglTNEuDc65Klo1jRONlg4qj7p>',
   });
 */
 
 //*****************************************************************
-			//Twitter
+      //Twitter
 //*****************************************************************
 //Twitter  
 
